7d91d5495e9aefa09a0d8ed393b27a1f3bfc1ee2
tuhoang <htu25399@gmail.com>
2023-01-21 18:35:09 +0700

    unit test article list component

--- src/app/shared/components/article-list/article-list.component.spec.ts
+++ src/app/shared/components/article-list/article-list.component.spec.ts
@@ -1,23 +1,69 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { render } from '@testing-library/angular';
+import { Article } from 'src/app/core/models';
+import { ArticleComponent } from '../article/article.component';
 
 import { ArticleListComponent } from './article-list.component';
 
-describe('ArticleListComponent', () => {
-  let component: ArticleListComponent;
-  let fixture: ComponentFixture<ArticleListComponent>;
+describe(ArticleListComponent.name, () => {
+  const mockedArticleList: Article[] = [
+    {
+      content:
+        'Share your knowledge and enpower the community by creating a new implementation',
+      creationTime: new Date(),
+      slug: 'Create-a-new-implementation-1',
+      tags: ['implementations'],
+      title: 'Create a new implementation',
+      userId: 1,
+      id: 1,
+      description:
+        'Share your knowledge and enpower the community by creating a new implementation',
+      favorited: 0,
+    },
+    {
+      content:
+        'Share your knowledge and enpower the community by creating a new implementation',
+      creationTime: new Date(),
+      slug: 'Create-a-new-implementation-1',
+      tags: ['implementations'],
+      title: 'Create a new implementation',
+      userId: 1,
+      id: 1,
+      description:
+        'Share your knowledge and enpower the community by creating a new implementation',
+      favorited: 0,
+    },
+  ];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ ArticleListComponent ]
-    })
-    .compileComponents();
+  async function setup(articleList: Article[] = []) {
+    return await render(ArticleListComponent, {
+      componentProperties: {
+        articleList,
+      },
+    });
+  }
 
-    fixture = TestBed.createComponent(ArticleListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('The create', async () => {
+    const { fixture } = await setup();
+    expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('When render', () => {
+    it('Show article list', async () => {
+      const { debugElement } = await setup(mockedArticleList);
+      const articleComponents = debugElement.queryAll(
+        By.directive(ArticleComponent)
+      );
+      expect(articleComponents.length).toEqual(2);
+    });
+    it('Show #noArticle when article list is empty', async () => {
+      const { debugElement } = await setup();
+      const noArticleElement = debugElement.query(By.css('#no-article'));
+      expect(noArticleElement).toBeTruthy();
+      expect(noArticleElement.nativeElement).toHaveTextContent(
+        'No articles are here... yet.'
+      );
+    });
   });
 });
--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,30 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { configure } from '@testing-library/angular';
+import '@testing-library/jasmine-dom';
+import JasmineDOM from '@testing-library/jasmine-dom';
+
+// Install custom matchers from jasmine-dom
+beforeEach(() => {
+  jasmine.addMatchers(JasmineDOM);
+});
+
+configure({
+  defaultImports: [ReactiveFormsModule],
+});
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     <T>(id: string): T;
     keys(): string[];
   };
@@ -17,7 +33,7 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
+  platformBrowserDynamicTesting()
 );
 
 // Then we find all the tests.
