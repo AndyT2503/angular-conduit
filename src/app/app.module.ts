70bd6d596d49be2489c09fd4da89968c7cc29a51
tuhoang <htu25399@gmail.com>
2023-01-22 11:56:52 +0700

    remove universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,23 @@
-import { TitleStrategy } from '@angular/router';
 import { APP_INITIALIZER, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, TitleStrategy } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { FooterComponent } from './core/layout/footer/footer.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { SeoService, TitleStrategyService } from './shared/services';
-import { TransferHttpCacheModule } from '@nguniversal/common'
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'real-world-conduit' }),
     HeaderComponent,
-    TransferHttpCacheModule,
     RouterModule,
     FooterComponent,
+    BrowserModule,
     RouterModule.forRoot(appRoutes, {
       scrollPositionRestoration: 'top',
       useHash: true,
-      initialNavigation: 'enabledBlocking',
     }),
   ],
   providers: [
@@ -35,8 +31,9 @@ import { TransferHttpCacheModule } from '@nguniversal/common'
       multi: true,
     },
     {
-      provide: TitleStrategy, useClass: TitleStrategyService
-    }
+      provide: TitleStrategy,
+      useClass: TitleStrategyService,
+    },
   ],
 })
 export class AppModule {}
